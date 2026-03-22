29780a53b004afd551aa8812de67870e83ea8a46
brittmrenlxqwnjehkzfsuocapvdyv <136777484+brittenpoon@users.noreply.github.com>
2026-03-22 01:31:35 -0400

    Update jalan.user.js

--- jalan.user.js
+++ jalan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jalan Helper - Auto Next & Intent Catcher
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      4.2
 // @description  Tab isolation, infinite memory, manual click recovery, and auto "Next" within 1 min of batch open
 // @author       Gemini
 // @match        *://www.jalan.net/*
